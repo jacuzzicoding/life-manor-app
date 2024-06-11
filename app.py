3b74fe6ef4206074133573f063ab4d194e5c089e
Brock Jenkinson <brockjenkinson@gmail.com>
2024-06-11 01:39:39 -0400

    chore: Add resource management app skeleton

---
+++ app.py
@@ -0,0 +1,10 @@
+#this will be a resource management app for your real life
+#it can be used to manage your resources like books, movies, food, money, etc
+
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+import spacy
+
+app = Flask(__name__)
